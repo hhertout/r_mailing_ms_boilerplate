b651d0f6596e8c41eee6759d5963d6aa89052aea
hhertout <hugues.hertout@gmail.com>
2023-10-13 20:48:58 +0200

    doc(api): fix doc comment

--- src/config/cors.rs
+++ src/config/cors.rs
@@ -3,5 +3,5 @@ use actix_cors::Cors;
 pub fn config_cors() -> Cors {
     Cors::permissive()
 
-    // TODO build a restricitve cors config for prod release (depend on env variable ?)
+    // TODO build a restricitive cors config for prod release (depend on env variable ?)
 }
