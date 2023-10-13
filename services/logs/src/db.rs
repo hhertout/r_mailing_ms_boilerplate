e7c2e060e6147f9efd733a8dfddc85df995babde
hhertout <hugues.hertout@gmail.com>
2023-10-13 20:47:51 +0200

    feat(logs): adding sqlite && logs services to handles mailing logs

---
+++ services/logs/src/db.rs
@@ -0,0 +1,50 @@
+use std::env;
+
+use sqlx::{migrate::MigrateDatabase, sqlite::SqlitePoolOptions, Pool, Sqlite};
+
+pub struct MailerDb;
+
+impl MailerDb {
+    pub fn new() -> MailerDb {
+        return MailerDb;
+    }
+
+    pub async fn database_connection(&self) -> Pool<Sqlite> {
+        let db_url = env::var("DB_URL").unwrap();
+        SqlitePoolOptions::new()
+            .connect(&db_url)
+            .await
+            .unwrap_or_else(|_| panic!("Failed to connect to database"))
+    }
+
+    pub async fn migrate(&self) {
+        let _ = &self.create_database().await;
+        let pool = &self.database_connection().await;
+        let _ = &self.create_logs_table(pool).await;
+    }
+
+    async fn create_database(&self) {
+        let db_url = env::var("DB_URL").unwrap();
+        if !Sqlite::database_exists(&db_url).await.unwrap_or(false) {
+            match Sqlite::create_database(&db_url).await {
+                Ok(_) => println!("Database successfully created"),
+                Err(e) => println!("Error during the creation of the database. {}", e),
+            };
+        }
+    }
+
+    async fn create_logs_table(&self, pool: &Pool<Sqlite>) {
+        let _ = sqlx::query(
+            "CREATE TABLE IF NOT EXISTS `logs` (
+                `_id` integer primary key AUTOINCREMENT not null,
+                `method` varchar(255) not null,
+                `to` varchar(255) not null, 
+                `date` date not null, 
+                `success` boolean not null
+        );",
+        )
+        .execute(pool)
+        .await
+        .unwrap();
+    }
+}
--- services/mailer/tests/config.rs
+++ services/mailer/tests/config.rs
@@ -1,38 +1,6 @@
-use mailer::config::Config;
+use std::env;
 
-#[test]
-pub fn env_from_test() {
-    let test_from = "noreply@test.fr";
-    std::env::set_var("SMTP_FROM", test_from);
-    assert_eq!(std::env::var("SMTP_FROM").unwrap(), test_from.to_string());
-}
-#[test]
-pub fn env_user_test() {
-    let test_user = "test";
-    std::env::set_var("SMTP_USER", test_user);
-    assert_eq!(std::env::var("SMTP_USER").unwrap(), test_user.to_string());
-}
-#[test]
-pub fn env_password_test() {
-    let test_password = "test";
-    std::env::set_var("SMTP_PASSWORD", test_password);
-    assert_eq!(
-        std::env::var("SMTP_PASSWORD").unwrap(),
-        test_password.to_string()
-    );
-}
-#[test]
-pub fn env_host_test() {
-    let test_host = "localhost";
-    std::env::set_var("SMTP_HOST", test_host);
-    assert_eq!(std::env::var("SMTP_HOST").unwrap(), test_host.to_string());
-}
-#[test]
-pub fn env_port_test() {
-    let test_port = "25";
-    std::env::set_var("SMTP_PORT", test_port);
-    assert_eq!(std::env::var("SMTP_PORT").unwrap(), test_port.to_string());
-}
+use mailer::config::Config;
 
 #[test]
 pub fn config_new() {
@@ -42,12 +10,12 @@ pub fn config_new() {
     let user = "toto";
     let password = "password";
     let from_name = "EXAMPLE";
-    std::env::set_var("SMTP_FROM", from);
-    std::env::set_var("SMTP_FROM_NAME", from_name);
-    std::env::set_var("SMTP_HOST", host);
-    std::env::set_var("SMTP_PORT", port);
-    std::env::set_var("SMTP_USER", user);
-    std::env::set_var("SMTP_PASSWORD", password);
+    env::set_var("SMTP_FROM", from);
+    env::set_var("SMTP_FROM_NAME", from_name);
+    env::set_var("SMTP_HOST", host);
+    env::set_var("SMTP_PORT", port);
+    env::set_var("SMTP_USER", user);
+    env::set_var("SMTP_PASSWORD", password);
 
     let config = Config::new();
 
@@ -66,19 +34,20 @@ pub fn config_new_without_from_name() {
     let port = "25";
     let user = "toto";
     let password = "password";
-    std::env::set_var("SMTP_FROM", from);
-    std::env::remove_var("SMTP_FROM_NAME");
-    std::env::set_var("SMTP_HOST", host);
-    std::env::set_var("SMTP_PORT", port);
-    std::env::set_var("SMTP_USER", user);
-    std::env::set_var("SMTP_PASSWORD", password);
+    env::remove_var("SMTP_FROM_NAME");
+    env::set_var("SMTP_FROM", from);
+    env::set_var("SMTP_HOST", host);
+    env::set_var("SMTP_PORT", port);
+    env::set_var("SMTP_USER", user);
+    env::set_var("SMTP_PASSWORD", password);
+
+    println!("{:?}", env::var("SMTP_FROM_NAME"));
 
     let config = Config::new();
-
     assert_eq!(config.from, from);
     assert_eq!(config.user, user);
     assert_eq!(config.password, password);
     assert_eq!(config.host, host);
     assert_eq!(config.port, port.parse::<u16>().unwrap());
-    assert_eq!(config.from_name, None)
+    assert_eq!(config.from_name, None);
 }
