01d4def2432c06e792079c34ad80bd956178adbc
hhertout <hugues.hertout@gmail.com>
2023-11-05 11:14:10 +0100

    chore: add global env for showing logs in dev but not in prod

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ pub mod config;
 
 #[actix_web::main]
 async fn main() -> Result<()>{
-    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
     dotenvy::dotenv().ok();
+    let env_mode = env::var("ENV").unwrap_or_else(|_| panic!("Failed to load global env variable"));
+    if  env_mode == "dev" {
+        println!("🔨 Running on dev mode");
+        env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    }
     api::init().await
 }
